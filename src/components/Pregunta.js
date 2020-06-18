2224dd9ca429214197b6069bdd0d6a39e8cf969d
fearARGENTINA <bravogermanezequiel@gmail.com>
2020-06-18 16:09:50 -0300

    Stateful implementacion - Se agrego funcionalidad para guardar el numero ingresado ante un cambio o submit - Se realiza validacion, si esta es erronea se activa un flag en el state

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    // Definir el State
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    // Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt(e.target.value) );
+    };
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+        if( cantidad < 1 || isNaN( cantidad ) ) {
+            guardarError(true);
+            return;
+        }
+
+        // Si pasa validacion
+        guardarError(false);
+    }
 
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+            
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
                 <input
                     type="submit"
